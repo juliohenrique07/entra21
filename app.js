74c4063b96ebc09ffefc27aef890a97bb43e0875
JULIO HENRIQUE PEREIRA <julio.pereira@alunos.sc.senac.br>
2023-09-01 21:04:53 -0300

    criacao do indexjs e alteracao no app

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log('ola mundo');
 console.log('Tudo bem');
+
 console.log('eu sou o homem de ferro');
---
+++ index.js
@@ -0,0 +1,4 @@
+function soma(nota1, nota2) {
+    return nota1 + nota2;
+}
+console.log(soma(5, 5));
